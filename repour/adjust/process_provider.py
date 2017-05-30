21a35b9380f6b89088f4034043e053dfaea96b2b
Dustin Kut Moy Cheung <dcheung@redhat.com>
2017-05-30 14:52:39 -0400

    Make PME logs shown in real-time

--- repour/adjust/process_provider.py
+++ repour/adjust/process_provider.py
@@ -29,19 +29,6 @@ def get_process_provider(execution_name, cmd, get_result_data=None, log_context_
         log_executable_info(cmd)
         filled_cmd = [p.format(repo_dir=repo_dir) if p.startswith("{repo_dir}") else p for p in cmd]
 
-        # log_context = getattr(asyncio.Task.current_task(), "log_context", None)
-        # if log_context_option is None:
-        #     if log_context is None:
-        #         env = None
-        #     else:
-        #         env = {
-        #             "LOG_CONTEXT": asyncio.Task.current_task().log_context,
-        #         }
-        # else:
-        #     env = None
-        #     if log_context is not None:
-        #         filled_cmd.append(log_context_option)
-        #         filled_cmd.append(log_context)
         env = None
         stdout = None
         try:
@@ -52,6 +39,7 @@ def get_process_provider(execution_name, cmd, get_result_data=None, log_context_
                 env=env,
                 stdout=stdout_options["lines"] if send_log else stdout_options["ignore"],
                 stderr=stderr_options["stdout"] if send_log else stderr_options["log_on_error"],
+                live_log=send_log,
             )
         except exception.CommandError as e:
             logger.error('Adjust subprocess failed, exited code "{e.exit_code}"'.format(**locals()))
@@ -60,12 +48,6 @@ def get_process_provider(execution_name, cmd, get_result_data=None, log_context_
         logger.info("Adjust subprocess exited OK!")
 
         adjust_result_data = {}
-        # TODO adjust_result_data["stdout"] = stdout
-        if stdout:
-            logger.info("Output:")
-            logger.info('\n'.join(stdout))
-        else:
-            logger.info("No Output")
 
         adjust_result_data["adjustType"] = execution_name
         adjust_result_data["resultData"] = yield from get_result_data(repo_dir)
