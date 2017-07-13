11ecc067f0bd34a2d5fdf892c1148d3375d60a40
Dustin Kut Moy Cheung <dcheung@redhat.com>
2017-07-13 12:11:56 -0400

    originRepoUrl is a dict

--- repour/server/endpoint/validation.py
+++ repour/server/endpoint/validation.py
@@ -47,7 +47,7 @@ adjust_raw = {
     "name": name_str,
     "ref": nonempty_str,
     Optional("sync"): bool,
-    Optional("originRepoUrl"): str,
+    Optional("originRepoUrl"): dict,
     Optional("adjustParameters"): All(dict),
     Optional("callback"): callback_raw,
 }
