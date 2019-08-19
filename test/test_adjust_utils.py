018df190d2770cee89683562a3e3b8211e739dac
Dustin Kut Moy Cheung <dcheung@redhat.com>
2019-08-19 16:56:57 -0400

    [NCL-5064] Handle more cases to parse params from PME
    This is done by using argparse to parse the versions.
    
    Cases tested:
    
    ```
    -f <file>
    --file=<file>
    --file <file>
    ```

---
+++ test/test_adjust_utils.py
@@ -0,0 +1,25 @@
+import unittest
+import repour.adjust.util as util
+
+class TestAdjustUtil(unittest.TestCase):
+
+    def test_util_file_option(self):
+
+        param = {"CUSTOM_PME_PARAMETERS": "-Dtest=test -f haha/pom.xml"}
+
+        remaining_args, filepath = util.get_extra_parameters(param)
+
+        self.assertEqual(remaining_args, ['-Dtest=test'])
+        self.assertEqual(filepath, "haha/")
+
+        param_file = {"CUSTOM_PME_PARAMETERS": "--file hoho/pom.xml -Dtest=test"}
+
+        remaining_args, filepath = util.get_extra_parameters(param_file)
+        self.assertEqual(remaining_args, ['-Dtest=test'])
+        self.assertEqual(filepath, "hoho/")
+
+        param_file_equal = {"CUSTOM_PME_PARAMETERS": "-Dtest2=test2 --file=hihi -Dtest=test"}
+
+        remaining_args, filepath = util.get_extra_parameters(param_file_equal)
+        self.assertEqual(remaining_args, ['-Dtest2=test2', '-Dtest=test'])
+        self.assertEqual(filepath, "hihi")
