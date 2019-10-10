169380735332e45add0614ce563549e8d3a714ba
Dustin Kut Moy Cheung <dcheung@redhat.com>
2019-10-10 09:44:27 -0400

    Specify java version for PME also

---
+++ test/test_util.py
@@ -0,0 +1,17 @@
+import unittest
+import repour.adjust.util as util
+
+class TestUtil(unittest.TestCase):
+
+
+    def test_get_jvm_from_extra_parameters(self):
+
+        extra_params = ['-DRepour_Java=1.8.0', 'not appropriate']
+        jvm = util.get_jvm_from_extra_parameters(extra_params)
+
+        self.assertEqual('1.8.0', jvm)
+
+        extra_params = ['not really', 'not appropriate']
+
+        jvm_not_specified = util.get_jvm_from_extra_parameters(extra_params)
+        self.assertEqual(None, jvm_not_specified)
