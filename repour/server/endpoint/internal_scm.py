e11e21453c72df65f41fca8a9889af2283eceb0f
Dustin Kut Moy Cheung <dcheung@redhat.com>
2020-10-09 17:24:10 -0400

    [NCL-5414] Add internal-scm endpoint
    The `/internal-scm` endpoint is used to create a new Gerrit repository.
    The request is:
    ```
    {
        "project": nonempty_noblank_str,
        "owner_groups": [nonempty_noblank_str],
        Optional("description"): null_or_str,
        Optional("parent_project"): null_or_str,
        Optional("callback"): callback_raw,
    }
    ```
    
    And the response is:
    ```
    {
        "status": "SUCCESS_CREATED",
        "readonly_url": "http://haha/project.git",
        "readwrite_url": "git+ssh://haha/project.git",
    }
     # ^^ if created
    
    {
        "status": "SUCCESS_ALREADY_EXISTS",
        "readonly_url": "http://haha/project.git",
        "readwrite_url": "git+ssh://haha/project.git",
    }
     # ^^ if already exists
    
    {
        "error_traceback": "id",
        "error_type": "error type",
        "log": "everything went wrong"
    }
     # ^^ if failure, returns status 500
    ```
    
    The creation is done by mimicking what was done before in our Maitai
    implementation. It is done by sshing into the Gerrit server and running
    the `gerrit create-project` command.
    
    The configuration change:
    ```
      ...
      "gerrit": {
          "username": "gerrit-user",
          "hostname": "<gerrit hostname>",
          "read_only_template": "http://<gerrit server>/gerrit/{REPO_NAME}.git",
          "read_write_template": "git+ssh://<gerrit server>/{REPO_NAME}.git"
      },
      ...
    ```

---
+++ repour/server/endpoint/internal_scm.py
@@ -0,0 +1,82 @@
+import logging
+
+from prometheus_async.aio import time
+from prometheus_client import Histogram, Summary
+from ...config import config
+
+import asyncssh
+
+
+REQ_TIME = Summary("internal_scm_req_time", "time spent with internal-scm endpoint")
+REQ_HISTOGRAM_TIME = Histogram(
+    "internal_scm_histogram", "Histogram for internal-scm endpoint"
+)
+
+logger = logging.getLogger(__name__)
+
+
+@time(REQ_TIME)
+@time(REQ_HISTOGRAM_TIME)
+async def internal_scm(spec, repo_provider):
+    """
+    spec is looks like validation.internal_scm
+
+    Output is:
+    => success: {"status": "SUCCESS_CREATED", "readonly_url": "..", "readwrite_url": ".."} if project created
+    => success: {"status": "SUCCESS_ALREADY_EXISTS", "readonly_url": "..", "readwrite_url": ".."} if project created
+    => failure: {"status": "FAILURE", "exit_status": <exit status:int>, "command_log": "<log: str>"}
+    """
+
+    configuration = await config.get_configuration()
+    configuration = configuration.get("gerrit")
+
+    readonly_url = configuration.get("read_only_template")
+    readwrite_url = configuration.get("read_write_template")
+
+    command = build_gerrit_command(
+        spec.get("project"),
+        spec.get("parent_project"),
+        spec.get("owner_groups"),
+        spec.get("description"),
+    )
+
+    logger.info("Command to run: " + command)
+
+    async with asyncssh.connect(
+        configuration.get("hostname"),
+        username=configuration.get("username"),
+        known_hosts=None,
+    ) as conn:
+
+        result = await conn.run(command, check=False)
+        exit_status = result.exit_status
+
+        if exit_status == 0:
+            return {
+                "status": "SUCCESS_CREATED",
+                "readonly_url": readonly_url.format(REPO_NAME=spec.get("project")),
+                "readwrite_url": readwrite_url.format(REPO_NAME=spec.get("project")),
+            }
+        elif exit_status == 1 and "Project already exists" in result.stderr:
+            return {
+                "status": "SUCCESS_ALREADY_EXISTS",
+                "readonly_url": readonly_url.format(REPO_NAME=spec.get("project")),
+                "readwrite_url": readwrite_url.format(REPO_NAME=spec.get("project")),
+            }
+        else:
+            raise Exception("Creation failed! Command log: " + result.stderr)
+
+
+def build_gerrit_command(project, parent_project, owner_groups, description):
+    command = "gerrit create-project '{}.git'".format(project)
+
+    if parent_project:
+        command += " -p '{}'".format(parent_project)
+
+    if description:
+        command += " -d '{}'".format(description)
+
+    for owner in owner_groups:
+        command += " -o '{}'".format(owner)
+
+    return command
---
+++ test/test_internal_scm.py
@@ -0,0 +1,26 @@
+# flake8: noqa
+import unittest
+import repour.server.endpoint.internal_scm as internal_scm
+
+
+class TestInternalSCM(unittest.TestCase):
+    def test_build_gerrit_command(self):
+        result = internal_scm.build_gerrit_command(
+            "project1",
+            "parent-project",
+            ["owner1", "owner2"],
+            "description1 description2",
+        )
+
+        # test if description specified
+        self.assertTrue("-d 'description1 description2'" in result)
+
+        # Test if owner parameters are specified
+        self.assertTrue("-o 'owner1'" in result)
+        self.assertTrue("-o 'owner2'" in result)
+
+        # Test if parent project specified
+        self.assertTrue("-p 'parent-project'" in result)
+
+        # Test if command invoked properly
+        self.assertTrue("gerrit create-project 'project1.git'" in result)
