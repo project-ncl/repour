446d7041812c4aa0a70c9df38c860f697ab7aec2
Dustin Kut Moy Cheung <dcheung@redhat.com>
2022-08-09 11:00:38 -0400

    [NCL-7263] Fix path to grab results

--- repour/adjust/process_provider.py
+++ repour/adjust/process_provider.py
@@ -82,7 +82,7 @@ def get_process_provider(
 
         adjust_result_data["adjustType"] = execution_name
         adjust_result_data["resultData"] = await get_result_data(
-            dir_results, extra_adjust_parameters, results_file=results_file
+            dir_where_results_are, extra_adjust_parameters, results_file=results_file
         )
 
         return adjust_result_data
