424933b4e89da626c33f1ca957d6bf6df09a3215
Petr Kočandrle <pkocandr@gmail.com>
2024-01-24 22:21:39 +0100

    [NCL-8044] Fix internal scm url handling issues

--- test/test_external_to_internal.py
+++ test/test_external_to_internal.py
@@ -11,33 +11,34 @@ loop = asyncio.get_event_loop()
 class TestExternalToInternal(unittest.TestCase):
     def test_translate_external_to_internal(self):
         c = loop.run_until_complete(config.get_configuration())
-        gerrit_server = c.get("git_url_internal_template", None)
+        for git_backend in ("gerrit", "gitlab"):
+            git_server = c.get(git_backend).get("git_url_internal_template", None)
 
-        valid_external_urls = [
-            "http://github.com/myproject/myrepo.git",
-            "https://github.com/myproject/myrepo/",
-            "git+ssh://github.com/myproject/myrepo.git",
-            "git@github.com:myproject/myrepo.git",
-            "git@github.com/myproject/myrepo.git",
-            "ssh://git@github.com/myproject/myrepo.git",
-        ]
+            valid_external_urls = [
+                "http://github.com/myproject/myrepo.git",
+                "https://github.com/myproject/myrepo/",
+                "git+ssh://github.com/myproject/myrepo.git",
+                "git@github.com:myproject/myrepo.git",
+                "git@github.com/myproject/myrepo.git",
+                "ssh://git@github.com/myproject/myrepo.git",
+            ]
 
-        invalid_external_urls = [
-            "://github.com/myproject/myrepo.git",
-            "github.com/myproject/myrepo.git",
-            "sh://github.com/myproject/myrepo.git",
-        ]
+            invalid_external_urls = [
+                "://github.com/myproject/myrepo.git",
+                "github.com/myproject/myrepo.git",
+                "sh://github.com/myproject/myrepo.git",
+            ]
 
-        internal_url = f"{gerrit_server}/myproject/myrepo.git"
+            internal_url = f"{git_server}/myproject/myrepo.git"
 
-        for external in valid_external_urls:
-            result = loop.run_until_complete(
-                external_to_internal.translate_external_to_internal(external)
-            )
-            self.assertEqual(result, internal_url)
-
-        for external in invalid_external_urls:
-            with self.assertRaises(Exception):
-                loop.run_until_complete(
+            for external in valid_external_urls:
+                result = loop.run_until_complete(
                     external_to_internal.translate_external_to_internal(external)
                 )
+                self.assertEqual(result, internal_url)
+
+            for external in invalid_external_urls:
+                with self.assertRaises(Exception):
+                    loop.run_until_complete(
+                        external_to_internal.translate_external_to_internal(external)
+                    )
