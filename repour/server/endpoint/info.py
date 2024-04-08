0f935b2635118b96cf1af60289e26a8129fd9a77
Dustin Kut Moy Cheung <dcheung@redhat.com>
2024-04-08 11:35:19 -0400

    Cache GIT_SHA for the info page
    For some strange reason git.rev_parse can be very slow sometimes.

--- repour/server/endpoint/info.py
+++ repour/server/endpoint/info.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 import json
+import logging
 import repour
 from aiohttp import web
 from prometheus_async.aio import time
@@ -14,16 +15,26 @@ from repour.lib.scm import git
 REQ_TIME = Summary("info_req_time", "time spent with info endpoint")
 REQ_HISTOGRAM_TIME = Histogram("info_req_histogram", "Histogram for info endpoint")
 
+logger = logging.getLogger(__name__)
+
+GIT_SHA = None
+
 
 @time(REQ_TIME)
 @time(REQ_HISTOGRAM_TIME)
 async def handle_request(request):
+    global GIT_SHA
+
     version = repour.__version__
     path_name = os.path.dirname(sys.argv[0])
-    try:
-        git_sha = await git.rev_parse(path_name)
-    except exception.CommandError:
-        git_sha = "Unknown"
+
+    # cache git sha output since the request can be slow
+    if GIT_SHA is None:
+        try:
+            logger.info("Getting the SHA for repour")
+            GIT_SHA = await git.rev_parse(path_name)
+        except exception.CommandError:
+            GIT_SHA = "Unknown"
 
     html_text = """
     <h1>Repour Information</h1>
