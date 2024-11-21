3d20f141f0309b4c792b4b7b64bc9f90310d17d0
Dustin Kut Moy Cheung <dcheung@redhat.com>
2024-11-21 14:30:53 -0500

    [NCLSUP-1136] Add pnc to the beginning or end if begin or end with '-'

--- test/test_internal_scm_gitlab.py
+++ test/test_internal_scm_gitlab.py
@@ -6,10 +6,10 @@ import repour.server.endpoint.internal_scm_gitlab as internal_scm_gitlab
 class TestInternalSCMGitlab(unittest.TestCase):
     def test_internal_scm_gitlab(self):
         result = internal_scm_gitlab.sanitize_gitlab_project_path("Qix-/color-convert")
-        self.assertEqual(result, "Qix_/color-convert")
+        self.assertEqual(result, "Qix_pnc/color-convert")
 
         result = internal_scm_gitlab.sanitize_gitlab_project_path("-Qix/color-convert")
-        self.assertEqual(result, "_Qix/color-convert")
+        self.assertEqual(result, "pnc_Qix/color-convert")
 
         result = internal_scm_gitlab.sanitize_gitlab_project_path("Qix/color-convert-")
-        self.assertEqual(result, "Qix/color-convert_")
+        self.assertEqual(result, "Qix/color-convert_pnc")
